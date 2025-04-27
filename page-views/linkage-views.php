9314b26202b916836508c2673060372c99c64340
asanul <asanulasanul05@gmail.com>
2025-04-27 17:34:54 +0800

    init

---
+++ page-views/linkage-views.php
@@ -0,0 +1,162 @@
+    <section class="ero-hero">
+        <div class="ero-text-cont">
+            <p class="subtitle">WESTERN MINDANAO STATE UNIVERSITY’S</p>
+            <h1 class="title">LINKAGES</h1>
+            <h1 class="title2">& PARTNERSHPS</h1> 
+        </div>
+        <div class="page-visit">
+            <div class="page-visit-circ"></div>
+            <div class="page-visit-count"><p>16,666 Page Visits</p></div>
+        </div>
+    </section>
+
+    <section class="internation-exchange-program">
+        <div class="iep-cont">
+            <div class="circ-cont">
+                <div><img src="../imgs/ero-iep-globe.png" alt=""></div>
+            </div>
+            <div class="iep-text-cont">
+                <div>
+                    <p class="iep-p">International Exchange Program</p>
+                    <p class="iep-p2">Apply now for our 2025 student exchange program with partner universities worldwide.</p>
+                </div>
+                <div class="apply-now-cont">
+                    <button class="apply-now-btn">Apply Now</button>
+                </div>
+                <div class="iep-three-dots">
+                    <div class="iep-three-circ" id="circ1"></div>
+                    <div class="iep-three-circ" id="circ2"></div>
+                    <div class="iep-three-circ" id="circ3"></div>
+                </div>
+            </div> 
+        </div>
+    </section>
+
+    <section class="our-global-network">
+        <div class="ogn-text">
+            <h1 class="ogn-h1">Our Global Network</h1>
+            <p class="ogn-p">Our university has established strong partnerships with leading institutions across the globe, creating <br>opportunities for research collaboration, student exchange, and academic innovation.</p>
+        </div>
+        <div class="ogn-cont">
+            <div><img src="../imgs/partners-map.png" alt=""></div>
+            <div class="ogn-label">
+                <div class="ogn-circ-blue"></div>
+                <div><p>Our University</p></div>
+                <div class="ogn-circ-red"></div>
+                <div><p>Our Partners</p></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="featured-partners">
+        <div>
+            <h1 class="featured-partners-h1">Featured Partnerships</h1>
+        </div>
+        <div class="featured-partners-cont"> 
+            <div class="featured-partners-box-left">
+                <div class="featured-partners-top"><img src="../imgs/ero-huawei.png" alt=""></div>
+                <div class="featured-partners-bot">
+                    <div class="featured-partners-bot-text1">
+                        <h4>Huawei PH</h1>
+                        <p id="featured-partners-p1">Taguig City, Metro Manila, Philippines</p>
+                    </div>
+                    
+                    <div class="featured-partners-bot-text2">
+                        <p id="featured-partners-p2">an exciting collaboration that typically focuses on technology development, digital skills training, and research initiatives in the field of information and communication technology (ICT). </p>
+                    </div>
+                
+                    <div class="featured-partners-bot-text3">
+                        <p id="featured-partners-p3"><b>Learn more about this partnership</b></p>
+                        <img src="../imgs/ero-arrow.png" alt="">
+                    </div>
+                
+                </div>
+            </div>
+
+            <div class="featured-partners-box-right">
+                <div class="featured-partners-top"><img src="../imgs/ero-seamo-innotech.png" alt=""></div>
+                <div class="featured-partners-bot">
+                    <div class="featured-partners-bot-text1">
+                        <h4>SEAMEO Innotech</h1>
+                        <p id="featured-partners-p1">Quezon City, Philippines</p>
+                    </div>
+                    
+                    <div class="featured-partners-bot-text2">
+                        <p id="featured-partners-p2">a regional educational organization that aims to promote innovation in education and development across Southeast Asia.  </p>
+                    </div>
+                
+                    <div class="featured-partners-bot-text3">
+                        <p id="featured-partners-p3"><b>Learn more about this partnership</b></p>
+                        <img src="../imgs/ero-arrow.png" alt="">
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="linkages-partnership-statistic">
+        <div><h1 class="linkages-partnership-statistic-h1">Linkages & Partnership Statistics</h1></div>
+        <div class="linkages-partnership-statistic-cont">
+
+            <div class="linkages-partnership-statistic-box">
+                <div class="linkages-partnership-statistic-box-top"><img src="../imgs/ero-stat-internation-partners-img.png" alt=""></div>
+                    <div class="lps-icon-cont">
+                        <div class="lps-icon-icon"><img src="../imgs/ero-stat-international-partners-icon.png" alt=""></div>
+                    </div>
+                <div class="linkages-partnership-statistic-box-bot">
+                    <div class="lps-h1"><h1>26</h1></div>
+                    <div class="lps-p"><p>International Partners</p></div>
+                </div>
+            </div>
+
+            <div class="linkages-partnership-statistic-box">
+                <div class="linkages-partnership-statistic-box-top"><img src="../imgs/ero-stat-overall-partners-img.png" alt=""></div>
+                    <div class="lps-icon-cont">
+                        <div class="lps-icon-icon"><img src="../imgs/ero-stat-overall-partners-icon.png" alt=""></div>
+                    </div>
+                <div class="linkages-partnership-statistic-box-bot">
+                    <div class="lps-h1"><h1>42</h1></div>
+                    <div class="lps-p"><p>Overall Partners</p></div>
+                </div>
+            </div>
+
+            <div class="linkages-partnership-statistic-box">
+                <div class="linkages-partnership-statistic-box-top"><img src="../imgs/ero-stat-national-partners-img.png" alt=""></div>
+                    <div class="lps-icon-cont">
+                        <div class="lps-icon-icon"><img src="../imgs/ero-stat-national-partners-icon.png" alt=""></div>
+                    </div>
+                <div class="linkages-partnership-statistic-box-bot">
+                    <div class="lps-h1"><h1>16</h1></div>
+                    <div class="lps-p"><p>National Partners</p></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    
+    <section class='our-partners'>
+        <b>OUR PARTNERS</b>
+         <br>
+        GAHIMO PA
+    </section>
+
+    <section class="become-a-partner">
+        <div class="become-a-partner-container">
+            <div class="partner-left">
+                <div><h1 class="director-title">ELIRO</h1></div>
+                <div><h2 class="director-name">DR. MARIO R. OBRA JR.</h2></div>
+                <div><p class="director-role">Director, Quality Management Office,<br> International Relations Office & External Linkages</p></div>
+                <div class="director-img"><img src="../imgs/sir-mario.png" alt=""></div>
+            </div>
+
+            <div class="partner-right">
+                <div><h2 class="become-partner-heading">Become a Partner</h2></div>
+                <div><p class="become-partner-description">Interested in establishing a partnership with our <br> university? We're always looking to expand our <br>network of collaborators.</p></div>
+                <button class="become-partner-button">Contact our Partnership Office</button>
+                <div class="icon-left-quote"><img src="../imgs/icon-left-quote.png" alt=""></div>
+                <div class="icon-right-quote"><img src="../imgs/icon-right-quote.png" alt=""></div>
+            </div>
+        </div>
+    </section>
